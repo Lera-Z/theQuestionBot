38cede0dfe6aca0fccf74324fb3974a09345dd1f
Valeriya Zelenkova <lerie483@gmail.com>
2017-07-21 00:42:33 +0300

    Add files via upload

--- bot_theQuestion.py
+++ bot_theQuestion.py
@@ -1,23 +1,28 @@
-from gensim.models import Word2Vec
-import re
-from lxml import html
+# from gensim.models import Word2Vec
+# import re
+# from lxml import html
 
 import numpy as np
 import pymorphy2
 import nltk
-from selenium import webdriver
-import time
-
-from collections import Counter
-from functools import lru_cache
-from annoy import AnnoyIndex
-import pandas as pd
-from sklearn.neighbors import NearestNeighbors
-
-df = pd.read_csv('/Users/Valeriya/Downloads/root/LABS/thequestion-hack/data/thequestion.csv')
-df = df.dropna()
-
+# from selenium import webdriver
+# import time
+# from urllib.parse import urlencode
 from gensim import models
+import telebot
+from urllib.parse import quote
+import pickle
+# from collections import Counter
+from functools import lru_cache
+# from annoy import AnnoyIndex
+# import pandas as pd
+# from sklearn.neighbors import NearestNeighbors
+import json
+import requests
+
+# df = pd.read_csv('/Users/Valeriya/Downloads/root/LABS/thequestion-hack/data/thequestion.csv')
+# df = df.dropna()
+# df.to_pickle('pickled_df.pickle')
 
 model = models.KeyedVectors.load_word2vec_format('/Volumes/Transcend/web_0_300_20.bin', binary=True)
 
@@ -44,7 +49,7 @@ def infinite(word):
     return parsed_word
 
 def get_vector(str):
-    words = nltk.word_tokenize(str)#re.sub("[^а-я]", " ", str, flags=re.IGNORECASE | re.UNICODE)
+    words = nltk.word_tokenize(str) #re.sub("[^а-я]", " ", str, flags=re.IGNORECASE | re.UNICODE)
     total_vector = np.zeros(300)
     total_count = 0
     for word in words:
@@ -61,14 +66,11 @@ def get_vector(str):
         total_vector /= total_count
 
     return total_vector
-
-
-import pickle
+#
 X = pickle.load(open( "/Users/Valeriya/Desktop/question_stop_word_dump.pkl", "rb" ))
+nbrs = pickle.load(open('/Users/Valeriya/Downloads/neighbors.pickle', 'rb'))
+df = pickle.load(open('/Users/Valeriya/Downloads/pickled_df.pickle', 'rb'))
 
-nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(X)
-
-import telebot
 tg_token = '420371977:AAEtl9DfXPR0RCqdlunPcKMnKYSc5I7cC9M'
 
 bot = telebot.TeleBot(tg_token)
@@ -82,30 +84,24 @@ def search_questions(message):
     vec = get_vector(str(message))
     distances, indices = nbrs.kneighbors(vec)
     fin = df.iloc[indices[0],1]
-    # print('found questions')
     return fin.to_string().split('    ')[1][:36]
 
 @bot.message_handler(content_types=['text'])
 
 def return_similar_question(question):
     # print('proceeded to returning questions')
-    main_link = 'https://thequestion.ru/ru/feed?sactive=true&squery='
+    main_link = 'https://thequestion.ru/search/questions?limit=20&offset=0&q='
     ques = search_questions(question)
-    search_link = main_link+ques
-    driver = webdriver.Firefox()
-    driver.get(search_link)
-    # print('driver ok')
-    time.sleep(5)
-    htmlSource = driver.page_source
-    
-    root = html.fromstring(htmlSource)
-    # print('got root')
-    final_link = root.xpath('//a[@class="search-questions-list__title"]/@href')
-    driver.close()
-    bot.send_message(question.chat.id, 'https://thequestion.ru'+ str(final_link[0]))
+    encoded = quote(ques)
+    search_link = main_link+encoded
+    res = requests.get(search_link)
+    try:
+        question_id = json.loads(res.text)['items'][0]['id']
+        final_link = 'Вам может быть интересен следующий ответ: http://thequestion.ru/questions/' + str(question_id)
+        bot.send_message(question.chat.id, final_link)
+    except:
+        bot.send_message(question.chat.id, 'Похожих вопросов не найдено')
 
-        
-        
+
 if __name__ == '__main__':
    bot.polling(none_stop=True)
-
